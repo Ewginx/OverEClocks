85849abfc76348ff4a8cd082bbd4bd2df6fb845d
Evgeniy <ewginx@gmail.com>
2023-10-28 17:31:57 +1000

    add translation files

---
+++ src/Translation/Translation_ru.cpp
@@ -0,0 +1,8 @@
+#include "Translation.h"
+
+std::map<String, String> settings_translation = {
+    {String("Auto"), String("Авто")}, 
+    {String("WiFi Password"), String("WiFi пароль")}
+    {String("WiFi SSID"), String("WiFi SSID")}
+    {String("WiFi Password"), String("WiFi пароль")}
+    };
