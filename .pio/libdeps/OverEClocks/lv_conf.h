1fd9d3e77ce456ef619a1a45fa5ee455b3db2e9b
Evgeniy <ewginx@gmail.com>
2023-10-28 23:52:09 +1000

    add new font montserrat_32

--- .pio/libdeps/OverEClocks/lv_conf.h
+++ .pio/libdeps/OverEClocks/lv_conf.h
@@ -362,7 +362,7 @@
 #define LV_FONT_MONTSERRAT_26 0
 #define LV_FONT_MONTSERRAT_28 0
 #define LV_FONT_MONTSERRAT_30 0
-#define LV_FONT_MONTSERRAT_32 1
+#define LV_FONT_MONTSERRAT_32 0
 #define LV_FONT_MONTSERRAT_34 0
 #define LV_FONT_MONTSERRAT_36 0
 #define LV_FONT_MONTSERRAT_38 0
@@ -385,7 +385,7 @@
 /*Optionally declare custom fonts here.
  *You can use these fonts as default font too and they will be available globally.
  *E.g. #define LV_FONT_CUSTOM_DECLARE   LV_FONT_DECLARE(my_font_1) LV_FONT_DECLARE(my_font_2)*/
-#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(font_Roboto120) LV_FONT_DECLARE(font_Roboto45)
+#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(font_Roboto120) LV_FONT_DECLARE(font_Roboto45) LV_FONT_DECLARE(montserrat_32)
 
 /*Always set a default font*/
 #define LV_FONT_DEFAULT &lv_font_montserrat_14
