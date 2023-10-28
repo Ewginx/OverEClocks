db2958dbe05e5bbbd1d52a34f20f2b34cddbf341
Evgeniy <ewginx@gmail.com>
2023-10-29 00:16:26 +1000

    fix day of week translation

--- src/Translation/Translation_ru.cpp
+++ src/Translation/Translation_ru.cpp
@@ -2,7 +2,7 @@
 #if LANGUAGE == 1
 
     const char *settings_translation[] = {"Светлая тема","Авто", "Город, код страны ISO-81118", "Город и\nкод страны", "WiFi SSID", "WiFi SSID", "WiFi пароль", "WiFi пароль", "AccessPoint IP:"};
-    const char *digital_clock_translation[] = {"Понедельник","Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+    const char *digital_clock_translation[] = {"Воскресенье","Понедельник","Вторник", "Среда", "Четверг", "Пятница", "Суббота" };
 #endif
 
 
