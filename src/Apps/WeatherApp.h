8cea205ff23d7818094019ef2fc6085c6d00e3b8
Evgeniy <ewginx@gmail.com>
2023-11-09 23:21:18 +1000

    implement weather api deserializing and add method to set labels text

--- src/Apps/WeatherApp.h
+++ src/Apps/WeatherApp.h
@@ -1,26 +1,44 @@
 #pragma once
 #include "Arduino.h"
-#include <HttpClient.h>
-#include <WiFiClient.h>
-#include <WiFi.h>
+#include "Config/Config.h"
 #include <ArduinoJson.h>
-#include"Config/Config.h"
+#include <HttpClient.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include "GUI/Weather.h"
 
-class WeatherApp
-{
-private:
+class WeatherApp {
+  private:
     short int port = 80;
+    String weather_url =
+        "/v1/forecast.json"; // ?key={API_KEY}&q={CITY}&aqi=no&lang={COUNTRY_CODE}
     Weather *weather;
     TaskHandle_t Weather_Task;
-public:
-    WiFiClient wifi;
-    HttpClient client = HttpClient(wifi, weather_url, port);
+    bool url_is_ready = false;
 
+  public:
+    WiFiClient wifi;
+    HttpClient client = HttpClient(wifi, "api.weatherapi.com", port);
+    // HttpClient client = HttpClient(wifi, "jsonplaceholder.typicode.com", port);
+
+    void setup_weather_url();
+    void deserialize_json_response(String &response);
+    void set_temperature(int temperature);
+    void set_feels_like(int temperature);
+    void set_weather_condition(const char *conditions);
+    void set_wind(double wind_speed, const char *wind_dir);
+    void set_humidity(int humidity);
+    void set_pressure(int pressure);
+    void set_precipitation_probability(int rain_probability, int snow_probability);
+    void set_max_min_temperatures(int max_temp, int min_temp);
+    // void set_visibility(char *visibility);
+    // void set_uv(char *uv);
+    void set_city_and_country_code(const char *city, const char* country_code);
+    // void set_hour_forecast();
+    // void set_weather_img(char *code);
     void create_weather_task();
     static void send_weather_request(void *parameter);
 
     WeatherApp(Weather *weather);
     ~WeatherApp();
 };
-
-
