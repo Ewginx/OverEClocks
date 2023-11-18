041f1f27cddc7b932f23bb555f51f9ced05103c2
Evgeniy <ewginx@gmail.com>
2023-11-18 11:38:12 +1000

    delete get_display from init_gui

--- src/GUI/GuiApp.cpp
+++ src/GUI/GuiApp.cpp
@@ -51,8 +51,6 @@ GuiApp::GuiApp(/* args */) {
 };
 
 void GuiApp::init_gui() {
-    lv_disp_t *disp = lv_disp_get_default();
-    // this->set_light_theme(disp);
     lv_disp_load_scr(digital_clock->digitalClockScreen);
 };
 
