60ed8117c055e0d2039062853c6dc19f6bfb148d
Evgeniy <ewginx@gmail.com>
2023-12-14 20:39:32 +1000

    separate BME280 logic in dedicated class

---
+++ src/Apps/MicroclimateApp.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Config/Config.h"
+#include "GUI/DockPanel.h"
+#include <Adafruit_BME280.h>
+#include <Arduino.h>
+
+class MicroclimateApp {
+  private:
+    Adafruit_BME280 _bme_sensor;
+    lv_timer_t *_bme_timer = NULL;
+    DockPanel *_dock_panel;
+
+  public:
+    void bme_timer_cb(lv_timer_t *timer);
+    bool begin();
+    MicroclimateApp(DockPanel *dock_panel);
+    ~MicroclimateApp();
+};
