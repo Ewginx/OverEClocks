7942780bcdfdaf918dde3ecfe0ad89263b357ae1
Evgeniy <ewginx@gmail.com>
2023-12-17 21:21:18 +1000

    change filesystem for server from sd card to spiffs

--- src/Apps/ServerApp.cpp
+++ src/Apps/ServerApp.cpp
@@ -1,20 +1,20 @@
 #include "ServerApp.h"
 
-ServerApp::ServerApp(/* args */) : server(port) {
-
+ServerApp::ServerApp(StateApp *state_app) : server(port) {
+    if (!SPIFFS.begin(true)) {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
     // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
     //           { request->send(200, "text/plain", "Hi! Welcome to OEClock."); });
 }
 void ServerApp::setup() {
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send(SD, "/index.html", "text/html");
+        request->send(SPIFFS, "/index.html", "text/html");
     });
-    server.serveStatic("/", SD, "/");
+    server.serveStatic("/", SPIFFS, "/");
     server.begin();
     ElegantOTA.begin(&server);
 }
-void ServerApp::run() {
-
-    ElegantOTA.loop();
-}
+void ServerApp::run() { ElegantOTA.loop(); }
 ServerApp::~ServerApp() {}
--- src/Apps/ServerApp.h
+++ src/Apps/ServerApp.h
@@ -1,17 +1,17 @@
+#include "StateApp.h"
 #include <ESPAsyncWebServer.h>
 #include <ElegantOTA.h>
-#include <SD.h>
-class ServerApp
-{
-private:
+#include "SPIFFS.h"
+
+class ServerApp {
+  private:
     short int port = 80;
     AsyncWebServer server;
+    StateApp *_state_app;
 
-public:
+  public:
     void setup();
     void run();
-    ServerApp();
+    ServerApp(StateApp *state_app);
     ~ServerApp();
 };
-
-
