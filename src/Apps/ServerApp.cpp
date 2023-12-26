9329861540861f91703cb4998a05cbc7d14d0cad
Evgeniy <ewginx@gmail.com>
2023-12-26 21:20:26 +1000

    add routes to server

--- src/Apps/ServerApp.cpp
+++ src/Apps/ServerApp.cpp
@@ -12,6 +12,36 @@ void ServerApp::setup() {
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
         request->send(SPIFFS, "/index.html", "text/html");
     });
+    server.on("/wifi", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
+    server.on("/weather", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
+    server.on("/brightness", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
+    server.on("/theme", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
+    server.on("/time", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
+    server.on("/debug", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(SPIFFS, "/index.html", "text/html");
+    });
+
+    server.on("/weather", HTTP_POST, [](AsyncWebServerRequest *request){
+        String message;
+        if (request->hasParam("weather", true)) {
+            // message = request->getParam("api_key", true)->value();
+            message = request->getParam("city", true)->value();
+            message = request->getParam("language", true)->value();
+        } else {
+            message = "No message sent";
+        }
+        request->send(200);
+    });
     server.serveStatic("/", SPIFFS, "/");
     server.begin();
     ElegantOTA.begin(&server);
