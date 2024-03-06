d7c51ee81cc7ca722c9763b308d685c6be0a7c85
Evgeniy <ewginx@gmail.com>
2024-03-06 21:35:13 +1000

    add a few methods to SoundApp

--- src/Apps/SoundApp.cpp
+++ src/Apps/SoundApp.cpp
@@ -21,48 +21,71 @@ void SoundApp::play_alarm_sound() {
         sound_loop_timer = lv_timer_create(loop_alarm_sound_cb_wrapper, 400, NULL);
         player.playFolderTrack(1, _state_app->sound_state->alarm_track_number);
         // Serial.println(player.getFolderTrackCount(1));
-        // Serial.println(player.getTotalFolderCount()); // doesn't work, at least with SD card
-        // Serial.println(player.getTotalTrackCount());
+        // Serial.println(player.getTotalFolderCount()); // doesn't work, at least with SD
+        // card Serial.println(player.getTotalTrackCount());
         // Serial.println(player.getCurrentTrack());
     }
     player.setVolume(_state_app->sound_state->volume_level);
 }
 
 void SoundApp::loop_alarm_sound() {
-    DfMp3_Status current_status = player.getStatus();
-    if (current_status.state == DfMp3_StatusState_Idle) {
-        player.playFolderTrack(1, _state_app->sound_state->alarm_track_number);
+    if (this->is_idling()) {
+        player.playFolderTrack(alarm_track_folder,
+                               _state_app->sound_state->alarm_track_number);
     }
 }
 
 void SoundApp::stop_sound() {
-    // player.stop();
-    // if (this->sound_loop_timer != NULL & !_state_app->alarm_state->alarm_ringing) {
-    //     lv_timer_del(this->sound_loop_timer);
-    //     this->sound_loop_timer = NULL;
-    // }
+    player.stop();
+    if (this->sound_loop_timer != NULL & !_state_app->alarm_state->alarm_ringing) {
+        lv_timer_del(this->sound_loop_timer);
+        this->sound_loop_timer = NULL;
+    }
 }
 
 void SoundApp::play_ee() {
     if (!_state_app->sound_state->sound_on || _state_app->alarm_state->alarm_ringing)
         return;
-    // player.stop();
-    // player.volume(_state_app->sound_state->volume_level);
-    // player.playFolder(2, _state_app->sound_state->ee_track_number);
+    player.setVolume(_state_app->sound_state->volume_level);
+    player.playFolderTrack(easter_egg_track_folder,
+                           _state_app->sound_state->ee_track_number);
 }
 
-// int SoundApp::get_file_counts() { return player.readFileCountsInFolder(1); }
+void SoundApp::play_sound_once(int folder, int track) {
+    player.playFolderTrack(folder, track);
+}
 
-void SoundApp::setup_player()  {
-    // Serial2.begin(9600);
+bool SoundApp::is_playing() {
+    DfMp3_Status current_status = player.getStatus();
+    if (current_status.state == DfMp3_StatusState_Playing) {
+        return true;
+    }
+    return false;
+}
+
+bool SoundApp::is_idling() {
+    DfMp3_Status current_status = player.getStatus();
+    if (current_status.state == DfMp3_StatusState_Idle) {
+        return true;
+    }
+    return false;
+}
+
+int SoundApp::get_track_count_in_alarm_folder() {
+    return player.getFolderTrackCount(alarm_track_folder);
+}
+
+int SoundApp::get_track_count_in_ee_folder() {
+    return player.getFolderTrackCount(easter_egg_track_folder);
+}
+
+void SoundApp::setup_player() {
     player.begin();
-    player.reset();
-    player.setVolume(
-        _state_app->sound_state->volume_level); // Set volume value. From 0 to 30
-    // player.printError();
+    player.reset(false);
+    player.setVolume(_state_app->sound_state->volume_level);
 }
 
-SoundApp::SoundApp(StateApp *state_app): player(Serial2) {
+SoundApp::SoundApp(StateApp *state_app) : player(Serial2) {
     instance = this;
     this->_state_app = state_app;
     lv_msg_subscribe(MSG_ALARM_PLAY, alarm_ringing_cb_wrapper, NULL);
--- src/Apps/SoundApp.h
+++ src/Apps/SoundApp.h
@@ -1,26 +1,33 @@
 #pragma once
 #include "Config/Config.h"
-// #include <DFPlayerMini_Fast.h>
 #include "StateApp.h"
 #include "lvgl.h"
 #include <Arduino.h>
 #include <DFMiniMp3.h>
-typedef DFMiniMp3<HardwareSerial> DfMp3; 
+
+typedef DFMiniMp3<HardwareSerial> DfMp3;
 
 class SoundApp {
   private:
     DfMp3 player;
-    // DFPlayerMini_Fast  player;
     lv_timer_t *sound_loop_timer = NULL;
+    short int alarm_track_folder = 1;
+    short int easter_egg_track_folder = 2;
 
   public:
-
     StateApp *_state_app;
     void play_alarm_sound();
     void loop_alarm_sound();
-    void stop_sound();
+
     void play_ee();
-    int get_file_counts();
+
+    void play_sound_once(int folder, int track);
+    bool is_playing();
+    bool is_idling();
+    void stop_sound();
+
+    int get_track_count_in_alarm_folder();
+    int get_track_count_in_ee_folder();
 
     void setup_player();
 
