82ff3b850dec96186bf9bca7200f554eceea72b1
Evgeniy <ewginx@gmail.com>
2024-03-13 00:49:20 +1000

    add method for playing plug in sound, fix hangout when connect to usb, decompose methods

--- src/Apps/SoundApp.cpp
+++ src/Apps/SoundApp.cpp
@@ -8,7 +8,10 @@ extern "C" void stop_sound_cb_wrapper(void *subscriber, lv_msg_t *msg) {
     instance->stop_sound();
 }
 extern "C" void play_ee_cb_wrapper(void *subscriber, lv_msg_t *msg) {
-    instance->play_ee();
+    instance->play_ee_sound();
+}
+extern "C" void play_plug_in_cb_wrapper(void *subscriber, lv_msg_t *msg) {
+    instance->play_plug_in_sound();
 }
 extern "C" void loop_alarm_sound_cb_wrapper(lv_timer_t *timer) {
     instance->loop_alarm_sound();
@@ -17,40 +20,56 @@ extern "C" void loop_alarm_sound_cb_wrapper(lv_timer_t *timer) {
 void SoundApp::play_alarm_sound() {
     if (!_state_app->sound_state->sound_on)
         return;
-    if (this->sound_loop_timer == NULL) {
-        player.reset(false); 
-        sound_loop_timer = lv_timer_create(loop_alarm_sound_cb_wrapper, 400, NULL);
-        player.playFolderTrack(1, _state_app->sound_state->alarm_track);
-        // Serial.println(player.getTotalFolderCount()); // doesn't work, at least with SD
-    }
+    this->reset_player();
+    lv_timer_resume(this->sound_loop_timer);
     this->set_volume();
+    player.playFolderTrack(1, _state_app->sound_state->alarm_track);
 }
 
 void SoundApp::loop_alarm_sound() {
-    this->set_volume();
-    if (this->is_idling()) {
-        player.playFolderTrack(alarm_track_folder, _state_app->sound_state->alarm_track);
+    if (this->is_player_offline()) {
+        this->stop_sound();
+        return;
+    };
+    if (this->is_playing()) {
+        return;
     }
+    this->set_volume();
+    player.playFolderTrack(alarm_track_folder, _state_app->sound_state->alarm_track);
 }
 
 void SoundApp::set_volume() { player.setVolume(_state_app->sound_state->volume_level); }
 
 void SoundApp::stop_sound() {
     player.stop();
-    if (this->sound_loop_timer != NULL & !_state_app->alarm_state->alarm_ringing) {
-        lv_timer_del(this->sound_loop_timer);
-        this->sound_loop_timer = NULL;
+    lv_timer_pause(this->sound_loop_timer);
+}
+
+void SoundApp::reset_player() {
+    if (this->need_to_reset) {
+        player.reset(false);
+        this->need_to_reset = false;
     }
 }
 
-void SoundApp::play_ee() {
-    if (!_state_app->sound_state->sound_on || _state_app->alarm_state->alarm_ringing)
-        return;
-    player.setVolume(_state_app->sound_state->volume_level);
-    player.playFolderTrack(easter_egg_track_folder, _state_app->sound_state->ee_track);
+void SoundApp::play_ee_sound() {
+    if (this->can_play_sounds(_state_app->sound_state->ee_sound_on))
+        this->play_sound_from_folder_once(easter_egg_track_folder,
+                                          _state_app->sound_state->ee_track);
 }
 
-void SoundApp::play_sound_once(int folder, int track) {
+void SoundApp::play_plug_in_sound() {
+    if (this->can_play_sounds(_state_app->sound_state->plug_sound_on))
+        this->play_sound_from_folder_once(easter_egg_track_folder,
+                                          _state_app->sound_state->plug_track);
+}
+
+void SoundApp::play_sound_from_folder_once(int folder, int track) {
+    if (this->is_player_offline()) {
+        return;
+    }
+    this->reset_player();
+    player.setVolume(_state_app->sound_state->volume_level);
     player.playFolderTrack(folder, track);
 }
 
@@ -70,6 +89,21 @@ bool SoundApp::is_idling() {
     return false;
 }
 
+bool SoundApp::is_player_offline() {
+    if (player.getTotalTrackCount() == 0) {
+        this->need_to_reset = true;
+        return true;
+    }
+    return false;
+}
+
+bool SoundApp::can_play_sounds(bool sound_on) {
+    if (_state_app->sound_state->sound_on & _state_app->alarm_state->alarm_ringing &
+        sound_on)
+        return true;
+    return false;
+}
+
 int SoundApp::get_track_count_in_alarm_folder() {
     return player.getFolderTrackCount(alarm_track_folder);
 }
@@ -79,6 +113,7 @@ int SoundApp::get_track_count_in_ee_folder() {
 }
 
 void SoundApp::setup_player() {
+
     player.begin();
     player.reset(false);
     player.setPlaybackSource(DfMp3_PlaySource_Sd);
@@ -88,9 +123,12 @@ void SoundApp::setup_player() {
 SoundApp::SoundApp(StateApp *state_app) : player(Serial2) {
     instance = this;
     this->_state_app = state_app;
+    this->sound_loop_timer = lv_timer_create(loop_alarm_sound_cb_wrapper, 900, NULL);
+    lv_timer_pause(this->sound_loop_timer);
     lv_msg_subscribe(MSG_ALARM_PLAY, alarm_ringing_cb_wrapper, NULL);
     lv_msg_subscribe(MSG_SOUND_STOP, stop_sound_cb_wrapper, NULL);
     lv_msg_subscribe(MSG_PLAY_EE, play_ee_cb_wrapper, NULL);
+    lv_msg_subscribe(MSG_PLAY_PLUG_IN, play_plug_in_cb_wrapper, NULL);
 }
 
 SoundApp::~SoundApp() {}
