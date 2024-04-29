291155fcd401bfe069654ab139c9f350092a3df1
Evgeniy <ewginx@gmail.com>
2024-04-30 01:17:02 +1000

    move delay for button to attribute

--- src/Apps/ButtonApp.cpp
+++ src/Apps/ButtonApp.cpp
@@ -9,8 +9,8 @@ extern "C" void measure_time_button_press_cb_wrapper(lv_timer_t *timer) {
 void ButtonApp::measure_time_button_press() {
     if (digitalRead(BUTTON_PIN) == HIGH) {
         Serial.println("BUTTON IS PRESSED");
-        this->elapsed_time += 200;
-        if (this->elapsed_time >= 4000) {
+        this->elapsed_time += this->measure_delay;
+        if (this->elapsed_time >= this->measure_delay * 10) {
             Serial.println("BUTTON WAS LONG PRESSED");
             this->was_pressed = true;
             this->elapsed_time = 0;
@@ -23,7 +23,7 @@ void ButtonApp::measure_time_button_press() {
         }
 
     } else {
-        if (this->elapsed_time >= 400 & !this->was_pressed) {
+        if (this->elapsed_time >= this->measure_delay * 2 & !this->was_pressed) {
             Serial.println("BUTTON WAS SHORT PRESSED");
             this->elapsed_time = 0;
             if (this->_state_app->alarm_state->alarm_ringing) {
@@ -43,7 +43,8 @@ ButtonApp::ButtonApp(StateApp *state_app) {
     instance = this;
     this->_state_app = state_app;
     pinMode(BUTTON_PIN, INPUT);
-    this->button_timer = lv_timer_create(measure_time_button_press_cb_wrapper, 200, NULL);
+    this->button_timer =
+        lv_timer_create(measure_time_button_press_cb_wrapper, this->measure_delay, NULL);
 }
 
 ButtonApp::~ButtonApp() {}
--- src/Apps/ButtonApp.h
+++ src/Apps/ButtonApp.h
@@ -7,6 +7,7 @@
 
 class ButtonApp {
   private:
+    int measure_delay = 300;
     int elapsed_time = 0;
     bool was_pressed = false;
     lv_timer_t *button_timer = NULL;
